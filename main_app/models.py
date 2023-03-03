99401bf2b1567c8def75dbf2f810b3d6307eba3d
mlkara <mlkarasek@gmail.com>
2023-03-03 17:49:43 -0500

    Add file1.txt

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class Moods(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+
+def __str__(self):
+    return f'{self.name} ({self.id})'
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'mood_id': self.id})
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('moods/', views.moods_index, name='index'),
+  path('moods/<int:mood_id>/', views.moods_detail, name='detail'),
 ]
