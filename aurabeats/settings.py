078a59e1124181f7d8bb28b0603b9327d89c39d0
mlkara <mlkarasek@gmail.com>
2023-03-07 12:20:48 -0500

    img

--- aurabeats/settings.py
+++ aurabeats/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS=[os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = '/moods/'
 LOGOUT_REDIRECT_URL = '/'
