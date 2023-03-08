48fdc84e63f3c409518f3bd548877530aa84166b
mlkara <mlkarasek@gmail.com>
2023-03-08 17:13:11 -0500

    Deployment1

--- aurabeats/settings.py
+++ aurabeats/settings.py
@@ -16,8 +16,7 @@ import environ
 environ.Env()
 environ.Env.read_env()
 
-import django_on_heroku
-django_on_heroku.settings(locals(), databases=False)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIRS=[os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = '/moods/'
 LOGOUT_REDIRECT_URL = '/'
@@ -136,5 +134,8 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals(), databases=False)
 
 
